2c451dc544d780b853b05fcd78b5be191f7f451d
zhangmuyang <279401783@qq.com>
2018-04-13 11:49:53 +0800

    first commit

---
+++ examples/802_15_4/wireless_uart/secure/second/pca10056/blank/arm5_no_packs/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'wireless_uart_secure_second_pca10056' 
+ * Target:  'nrf52840_xxaa' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
